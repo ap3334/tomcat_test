4125da8bcf98981aa98a0941f20c90fe885598c7
ap3334 <chwil2989@naver.com>
2022-07-21 13:41:20 +0900

    Rq 클래스 도입

---
+++ src/main/java/com/ll/exam/Rq.java
@@ -0,0 +1,53 @@
+package com.ll.exam;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Rq {
+
+    HttpServletRequest req;
+    HttpServletResponse resp;
+
+    public Rq(HttpServletRequest req, HttpServletResponse resp) {
+
+        this.req = req;
+        this.resp = resp;
+
+        try {
+            req.setCharacterEncoding("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+
+        resp.setCharacterEncoding("UTF-8");
+
+        resp.setContentType("text/html; charset=utf-8");
+
+    }
+
+    public int getIntParam(String paramName, int defaultValue) {
+
+        String paramValue = req.getParameter(paramName);
+
+        if (paramValue == null) {
+            return defaultValue;
+        }
+
+        try {
+            return Integer.parseInt(paramValue);
+        }
+        catch (NumberFormatException e) {
+            return defaultValue;
+        }
+
+    }
+
+    public void appendBody(String formatted) throws IOException {
+        resp.getWriter().append(formatted);
+    }
+}
